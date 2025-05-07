07d9eb86dd002eca1dfb44deb4aa71868cda4147
Dehoux <dehoux.kevin@gmail.com>
2025-05-07 15:25:53 +0200

    wip

---
+++ app/App.tsx
@@ -0,0 +1,13 @@
+import './global.css';
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+const Tab = createBottomTabNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+    </NavigationContainer>
+  );
+}
